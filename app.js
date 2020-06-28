84a930824df392b2ffa3eb41b3f058d791968c62
Keerthana Aluri <t-kealur@microsoft.com>
2020-06-28 14:39:22 -0400

    parsed json to a table

--- app.js
+++ app.js
@@ -29,36 +29,8 @@ app.post("/all_crimes", async (req, res) => {
     var api_url = front + selected_state + end;
 
     const fetch_response = await fetch(api_url);
-    const json = await fetch_response.json();
-    res.json(json);
-
-
-
-    //KEERTHANA ADDED THIS PART
-  //  var fs = require('fs');
-
-    console.log("\n *STARTING* \n");
-// Get content from file
-    // var contents = fs.readFileSync(json);
-// Define to JSON type
-
-    var jsonContent = JSON.parse(json);
-// Get Value from JSON
-    res.send("Year:", jsonContent.data_year);
-    res.send("Buying&Receiving:", jsonContent.buying__receiving);
-    res.send("Cultivating/Manufacturing/Publishing:", jsonContent.cultivating__manufacturing__publishing);
-    res.send("Distributing&Selling:",jsonContent.distributing__selling);
-    res.send("Exploting Children:", jsonContent.exploiting_children);
-    res.send("Operating/Promoting/Assisting:", jsonContent.operating__promoting__assisting);
-    res.send("Possessing/Concealing:", jsonContent.possessing__concealing);
-    res.send("Transporting/Transmitting/Importing:", jsonContent.transporting__transmitting__importing);
-    res.send("Using/Consuming:", jsonContent.using__consuming);
-    res.send("None/Unknown:", jsonContent.none__unknown);
-    res.send("Other Gang:",jsonContent.other_gang);
-    res.send("Juvenile Gang:",jsonContent.juvenile_gang);
-    res.send("Simple Gross Neglect:",jsonContent.simple__gross_neglect);
-    res.send("Organized Animal Abuse:", jsonContent.organized_aninal_abuse);
-    res.send("Intentional Abuse and Torture:", jsonContent.intentional_abuse_and_torture);
-    res.send("Animal Sexual Abuse:",jsonContent.animal_sexual_abuse);
-
+    const json = await fetch_response.json(); 
+    const tableify= require('tableify');
+    const table= tableify(json);
+    res.send(table);
 });
