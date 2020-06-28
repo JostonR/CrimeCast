31787c0dd7e219400b518a3b59b19ca2ff575961
Joston Rodrigues <t-josrod@microsoft.com>
2020-06-27 21:30:52 -0700

    api call is working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require('express');
 const app = express();
 const body_parser = require('body-parser');
 const request = require('request');
+const fetch = require('node-fetch');
 
 app.use(body_parser.urlencoded({extended: false}));
 app.use(express.static('./public'));
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/all_crimes", (req, res) => {
+app.get("/all_crimes", async (req, res) => {
     console.log("trying to find crimes");
     selected_state = req.body.state;
     console.log(selected_state);
@@ -25,27 +26,9 @@ app.get("/all_crimes", (req, res) => {
     var state = selected_state; 
     var front = "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/all-offenses/offense/states/";
     var end = "/CRIMINAL_ACTIVITY?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
-    //res.send(front + HI + end);
-//     request(front + "HI" + end, { json: true }, (err, res, body) => {
-//   if (err) { return console.log(err); }
-//   console.log(body.url);
-//   console.log(body.explanation);
-//    //res.send("hi");
-// });
-const axios = require('axios');
+    var api_url = front + "MI" + end;
 
-axios.get('https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/all-offenses/offense/states/HI/CRIMINAL_ACTIVITY?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv')
-  .then(response => {
-    res.send("WORK!!");
-    console.log(response.data.url);
-    console.log(response.data.explanation);
-    res.send("WORK!!");
-    res.setHeader(response.data.url);
-
-  })
-  .catch(error => {
-    console.log(error);
-    res.send("WORK!!");
-  });
-    res.end();
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    res.json(json);
 });
