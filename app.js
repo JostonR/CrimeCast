bd163b20e474bfa22f311c9b26d95a92d2a966ee
Joston Rodrigues <t-josrod@microsoft.com>
2020-06-27 18:38:00 -0700

    static page, retrieves state input

---
+++ app.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const app = express();
+const body_parser = require('body-parser');
+
+app.use(body_parser.urlencoded({extended: false}));
+app.use(express.static('./public'));
+
+app.listen(3000, () => {
+    console.log("Server is starting");
+});
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+
+app.get("/all_crimes", (req, res) => {
+    console.log("trying to find crimes");
+    selected_state = req.body.state;
+    console.log(selected_state);
+    res.end();
+});
